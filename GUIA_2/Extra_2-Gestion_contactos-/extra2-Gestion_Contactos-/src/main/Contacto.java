51bd71e83ebde496e704c1d5c58ef3a53b9963c5
FarielAce <f.ariel.acevedo@gmail.com>
2023-08-20 16:50:48 -0300

    comienzo de Extra2
    Primer subida del ejericio extra 2
    en el extra 1 se agrego el metodo organizaCabecera() que hace que tocando  la cabecera de la columna ordene los items.

---
+++ GUIA_2/Extra_2-Gestion_contactos-/extra2-Gestion_Contactos-/src/main/Contacto.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author ferna
+ */
+public class Contacto {
+    private String nombre;
+    private String apellido;
+    private int numero;
+    private String eMail;
+
+    public Contacto() {
+    }
+
+    public Contacto(String nombre, String apellido, int numero, String eMail) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numero = numero;
+        this.eMail = eMail;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+    
+}
